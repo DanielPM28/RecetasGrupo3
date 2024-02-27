43e57dab1fbe3bde789acb640bb985df8c7b52b9
DanielPM28 <153576338+DanielPM28@users.noreply.github.com>
2024-02-27 08:35:21 -0500

    Nav y carousel falta css

--- recetasgrupo3/src/Pages/App/Index.jsx
+++ recetasgrupo3/src/Pages/App/Index.jsx
@@ -1,13 +1,13 @@
 import { useRoutes , BrowserRouter } from 'react-router-dom';
 import Home from '../Home/Index'
-import Recetas from '../Recetas/Index'
 import NavBar from '../../Components/Navbar/Index'
+import RecipeCarousel from "../Recetas/Index";
 import './App.css'
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path:'/', element:<Home />},
-    { path: '/Recetas', element: <Recetas /> },
+    { path: '/Recetas', element: <RecipeCarousel /> },
   ])
   return routes
 }
@@ -15,10 +15,18 @@ const AppRoutes = () => {
 const App = () => {
 
   return (
+    <>
     <BrowserRouter>
       <AppRoutes />
       <NavBar />
     </BrowserRouter>
+    <div className="h-40 w-40 rounded-full my-auto mx-auto items-center justify-center">
+        <h1 className="text-3xl font-bold mb-4">Carrusel de Recetas</h1>
+        <RecipeCarousel />
+      </div>
+    </>
+    
+    
   )
 }
 export default App
--- recetasgrupo3/src/Pages/Recetas/Index.jsx
+++ recetasgrupo3/src/Pages/Recetas/Index.jsx
@@ -1,7 +1,45 @@
-export default function Developers() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Aqui encontraras las recetas que necesites!
-      </h1>
-    )
-  }
+import React from 'react';
+import Slider from 'react-slick';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+const RecipeCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 200,
+    autoplaySpeed: 5000,
+    autoplay: true,
+    slideToShow: 3,
+    slideToScroll: 1,
+  };
+
+  return (
+    
+          <Slider {...settings}>
+            {recipes.map((d) => (
+              <div key={d.title} className="">
+                <div className="text-black rounded-xl h-40 w-40 ">
+                  <img
+                    src={d.image}
+                    alt=""
+                    className="h-40 w-40  my-auto mx-auto items-center "
+                  />
+                </div>
+                
+              </div>
+            ))}
+          </Slider>
+  )
+};
+const recipes = [
+  { title: 'Receta 1', image: 'src/img/pasteles.jpg' },
+  { title: 'Receta 2', image: 'src/img/verduras.jpg' },
+  { title: 'Receta 3', image: 'src/img/pasteles.jpg' }
+  // Agrega más recetas según sea necesario
+];
+
+export default RecipeCarousel;
